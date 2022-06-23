76af51576775b66d78bdcd291131fda8b71aa6ca
Rodolfo Leal Braga <75742317+rodox021@users.noreply.github.com>
2022-06-22 22:50:47 -0300

    Adicionar arquivos de projeto.

---
+++ curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using curso.api.Models.Usuario;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [Route("logar")]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
+        {
+            return Created("", registroViewModelInput);
+        }
+    }
+}
---
+++ curso.api/Models/Usuario/RegistroViewModelInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Models.Usuario
+{
+    public class RegistroViewModelInput
+    {
+        [Required(ErrorMessage ="O Campo {0} é Obrigatório")]
+        public string  Login { get; set; }
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        public string Senha { get; set; }
+    }
+}
